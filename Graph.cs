1011e5c3c9bd78cde9b82c058946ffc9fc43a423
swathix <technekk66@gmail.com>
2017-12-07 17:11:47 -0500

    Add files via upload

---
+++ Graph.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphs
+{
+
+    public class Node
+    {
+        public string name;
+        public ArrayList edges = new ArrayList();
+        public Node(string name)
+        {
+
+            this.name = name;
+
+        }
+
+
+        public void addEdge(Node node)
+        {
+            Console.WriteLine(this.name);
+            this.edges.Add(node);
+            //foreach (Node i in this.edges)
+            //{
+
+            //    Console.WriteLine(i.name);
+            //}
+            //Console.WriteLine();
+        }
+
+    
+       
+        public void resolve()
+        {
+            Console.WriteLine(this.name);
+            foreach(Node edge in this.edges)
+            {
+                edge.resolve();
+            }
+
+        }
+
+
+        public void resolve(ArrayList resolved)
+        {
+            Console.WriteLine(this.name);
+            foreach (Node edge in this.edges)
+            {
+                edge.resolve( resolved);
+                resolved.Add(this.name);
+            }
+        }
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graphs
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            ArrayList resolved = new ArrayList();
+            Node a = new Node("a");
+            Node b = new Node("b");
+            Node c = new Node("c");
+            Node d = new Node("d");
+            Node e = new Node("e");
+            a.addEdge(b);
+            a.addEdge(d);
+            b.addEdge(c);
+            b.addEdge(e);
+            c.addEdge(d);
+            c.addEdge(e);
+            a.resolve();
+            a.resolve(resolved);
+            foreach(Node i in resolved)
+            {
+                Console.WriteLine(i.name);
+            }
+
+        }
+    }
+}
